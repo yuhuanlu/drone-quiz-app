77adc2d6afb82239220b1ea246e3a8e47543de92
eva.lu <eva.lu@keyxentic.com>
2025-06-06 17:07:05 +0800

    chore: next

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+const isGithubPages = process.env.NODE_ENV === 'production'
 
+const repo = 'drone-quiz-app' // <- 換成你的 repo 名稱
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export', // 啟用 static export
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '',
 };
 
 export default nextConfig;
